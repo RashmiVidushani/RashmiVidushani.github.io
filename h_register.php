5c00ad218da6abf252f06f7bf4d18de294899d92
Rashmi <rashmividushani2020@gmail.com>
2022-03-04 23:58:28 -0800

    ss

--- h_register.php
+++ h_register.php
@@ -1,3 +1,135 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+session_start();
+$username = $registration = $email = $mobile = $password = $confirm_password = "";
+$username_err = $registration_err = $email_err= $mobile_err = $password_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+
+        $username_err = "Please enter a username.";
+    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+        $username_err = "Username can only contain letters, numbers, and underscores.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT registration FROM hospital_base WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    // Validate registration
+    if(empty(trim($_POST["registration"]))){
+      $registration_err = "Please enter a registration number.";
+  } elseif(!preg_match('/^[a-zA-Z0-9]+$/', trim($_POST["registration"]))){
+      $registration_err = "registration can only contain letters, numbers";
+  } else{
+     $registration=trim($_POST["registration"]);
+  }
+    // Validate email
+    if(empty(trim($_POST["email"]))){
+      $email_err = "Please enter a email.";
+  } elseif(!preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i', trim($_POST["email"]))){
+      $email_err = "Invalid email";
+  } else{
+     $email=trim($_POST["email"]);
+  }
+      // Validate password
+      if(empty(trim($_POST["mobile"]))){
+        $mobile_err = "Please enter a mobile number.";     
+    } elseif(strlen(trim($_POST["mobile"])) < 10){
+      //check string lenght
+        $mobile_err = "mobile number must have atleast 10 characters.";
+    } else{
+        $mobile = trim($_POST["mobile"]);
+    }
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+      //check string lenght
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    /*Error*/
+
+
+    // Check input errors before inserting in database
+    if(empty($username_err) && empty($registration_err) && empty($email_err) && empty($mobile_err) && empty($password_err) && empty($confirm_password_err)){
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO hospital_base (username,registration,email,mobile, password) VALUES (?, ?, ? ,? ,?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_username,$param_registration,$param_email, $param_mobile, $param_password);
+            
+            // Set parameters
+            $param_username = $username;
+            $param_registration = $registration;
+            $param_email = $email;
+            $param_mobile= $mobile;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+     /*Error*/
+
+
+     
+    // Close connection
+    mysqli_close($link);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -25,36 +157,43 @@
             <div class="card col-lg-4 mx-auto">
               <div class="card-body px-5 py-5">
                 <h3 class="card-title text-left mb-3">Register</h3>
-                <form>
-                  <div class="form-group">
-                    <label>Registration No. *</label>
-                    <input type="text" class="form-control p_input">
-                  </div>
-                  <div class="form-group">
-                    <label>Email *</label>
-                    <input type="email" class="form-control p_input">
-                  </div>
-                  <div class="form-group">
-                    <label>Phone *</label>
-                    <input type="phone" class="form-control p_input">
-                  </div>
-                  <div class="form-group">
-                    <label>Password *</label>
-                    <input type="password" class="form-control p_input">
-                  </div>
-                  <div class="form-group">
-                    <label>Confirm password *</label>
-                    <input type="password" class="form-control p_input">
-                  </div>
-                  <div class="form-group d-flex align-items-center justify-content-between">
-                    <a href="#" class="forgot-pass">Forgot password</a>
-                  </div>
-                  <div class="text-center">
-                    <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
-                  </div>
-                  <p class="sign-up text-center">Already have an Account?<a href="login.html"> Sign Up</a></p>
-                  <p class="terms">By creating an account you are accepting our<a href="#"> Terms & Conditions</a></p>
-                </form>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>  
+            <div class="form-group">
+                <label>Registration number</label>
+                <input type="text" name="registration" class="form-control <?php echo (!empty($registration_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $registration; ?>">
+                <span class="invalid-feedback"><?php echo $registration_err; ?></span>
+            </div>  
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+                <span class="invalid-feedback"><?php echo $email_err; ?></span>
+            </div> 
+            <div class="form-group">
+                <label>Mobile</label>
+                <input type="text" name="mobile" class="form-control <?php echo (!empty($mobile_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $mobile; ?>">
+                <span class="invalid-feedback"><?php echo $mobile_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+            </div>
+            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        </form>
               </div>
             </div>
           </div>
